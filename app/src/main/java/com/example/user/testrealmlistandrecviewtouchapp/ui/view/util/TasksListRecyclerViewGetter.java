1bd797b77a06140aa0d5a61c151fa00c8eae9502
newbm2013 <Qaor25520>
2018-01-19 16:19:53 +0300

    +++

--- app/src/main/java/com/example/user/testrealmlistandrecviewtouchapp/ui/view/util/TasksListRecyclerViewGetter.java
+++ app/src/main/java/com/example/user/testrealmlistandrecviewtouchapp/ui/view/util/TasksListRecyclerViewGetter.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,6 +25,7 @@ public class TasksListRecyclerViewGetter {
     RecyclerView tasksListRecyclerView;
     TasksListRecyclerViewAdapter tasksListRecyclerViewAdapter;
     ArrayList arrayList;
+    boolean fingerUp;
 
 
     public TasksListRecyclerViewGetter(AppCompatActivity activity){
@@ -57,9 +59,29 @@ public class TasksListRecyclerViewGetter {
 
 
 
+//        final View.OnTouchListener touchListener = new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View view, MotionEvent motionEvent) {
+//
+//                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+//                    fingerUp = true;
+//                }
+//
+//                else if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) fingerUp = false;
+//
+//                return true;
+//            }
+//        };
+
+//        tasksListRecyclerView.setOnTouchListener(touchListener);
+
+
         ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(
                 ItemTouchHelper.UP| ItemTouchHelper.DOWN,
                 ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+
+
 
             //not used, as the first parameter above is 0
             @Override
@@ -69,27 +91,54 @@ public class TasksListRecyclerViewGetter {
 
 
 
+                recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+                    @Override
+                    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                        return false;
+                    }
+
+                    @Override
+                    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+                        if (e.getAction() == MotionEvent.ACTION_UP){
+                            fingerUp = true;
+                        }
+
+                    }
+
+                    @Override
+                    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+                    }
+                });
+
 
                 int i = target.getAdapterPosition();
-
-
-
                 int i2 = viewHolder.getAdapterPosition();
 
+                if (fingerUp){
 
+                    arrayList.set(i, arrayList.get(i2));
+                    tasksListRecyclerViewAdapter.notifyItemMoved(i2, i);
+                    String s = "";
+                    for (int ii=0; ii<arrayList.size(); ii++ ) s += arrayList.get(ii);
+                    Log.d("DTAG", "onMove: " + s);
+                }
 
-                arrayList.set(i, arrayList.get(i2));
                 tasksListRecyclerViewAdapter.notifyItemMoved(i2, i);
-                String s = "";
-                for (int ii=0; ii<arrayList.size(); ii++ ) s += arrayList.get(ii);
-                Log.d("DTAG", "onMove: " + s);
 
 
 
 
-                return false;
+
+
+                return true;
             }
 
+            @Override
+            public void onMoved(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, int fromPos, RecyclerView.ViewHolder target, int toPos, int x, int y) {
+//                super.onMoved(recyclerView, viewHolder, fromPos, target, toPos, x, y);
+            }
 
             @Override
             public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
