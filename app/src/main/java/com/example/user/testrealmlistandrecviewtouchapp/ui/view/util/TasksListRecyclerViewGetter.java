649d68880b5e4e849be717adbd281aeec48ea335
ShumidubCoder <shumidubcoder@gmail.com>
2018-01-20 06:52:35 +0300

    make good working touchhelper for recyclerview - now arrayList.set() work only then need once

--- app/src/main/java/com/example/user/testrealmlistandrecviewtouchapp/ui/view/util/TasksListRecyclerViewGetter.java
+++ app/src/main/java/com/example/user/testrealmlistandrecviewtouchapp/ui/view/util/TasksListRecyclerViewGetter.java
@@ -1,31 +1,28 @@
 package com.example.user.testrealmlistandrecviewtouchapp.ui.view.util;
 
 
-import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.user.testrealmlistandrecviewtouchapp.R;
 import com.example.user.testrealmlistandrecviewtouchapp.ui.util.recyclerviewadapter.TasksListRecyclerViewAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TasksListRecyclerViewGetter {
-
 
     AppCompatActivity activity;
     LayoutInflater layoutInflater;
     RecyclerView tasksListRecyclerView;
     TasksListRecyclerViewAdapter tasksListRecyclerViewAdapter;
     ArrayList arrayList;
-    boolean fingerUp;
+    Object arrayListCopy;
 
 
     public TasksListRecyclerViewGetter(AppCompatActivity activity){
@@ -36,125 +33,65 @@ public class TasksListRecyclerViewGetter {
         this.arrayList = arrayList;
         layoutInflater = activity.getLayoutInflater();
         tasksListRecyclerView = (RecyclerView) layoutInflater.inflate(R.layout.tasks_list_recycler_view, viewGroup, false);
-//        tasksListRecyclerView = view.findViewById(R.id.rv);
         tasksListRecyclerViewAdapter = new TasksListRecyclerViewAdapter(arrayList, activity);
         tasksListRecyclerView.setLayoutManager(new LinearLayoutManager(activity));
         tasksListRecyclerView.setAdapter(tasksListRecyclerViewAdapter);
-
-
+        arrayListCopy=arrayList.clone();
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(createHelperCallback());
         itemTouchHelper.attachToRecyclerView(tasksListRecyclerView);
-
-
         return tasksListRecyclerView;
     }
 
 
     @SuppressWarnings("all")
     private ItemTouchHelper.Callback createHelperCallback() {
-        /*First Param is for Up/Down motion, second is for Left/Right.
-        Note that we can supply 0, one constant (e.g. ItemTouchHelper.LEFT), or two constants (e.g.
-        ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) to specify what directions are allowed.
-        */
-
-
-
-//        final View.OnTouchListener touchListener = new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
-//
-//                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
-//                    fingerUp = true;
-//                }
-//
-//                else if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) fingerUp = false;
-//
-//                return true;
-//            }
-//        };
-
-//        tasksListRecyclerView.setOnTouchListener(touchListener);
-
 
         ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(
                 ItemTouchHelper.UP| ItemTouchHelper.DOWN,
                 ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
 
+            int dragFrom = -1;
+            int dragTo = -1;
 
 
-
-            //not used, as the first parameter above is 0
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
                                   RecyclerView.ViewHolder target) {
 
+                int fromPosition = viewHolder.getAdapterPosition();
+                int toPosition = target.getAdapterPosition();
 
+                if(dragFrom == -1) {dragFrom =  fromPosition;}
+                dragTo = toPosition;
 
-
-                recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
-                    @Override
-                    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-                        return false;
-                    }
-
-                    @Override
-                    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-
-                        if (e.getAction() == MotionEvent.ACTION_UP){
-                            fingerUp = true;
-                        }
-
-                    }
-
-                    @Override
-                    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-                    }
-                });
-
-
-                int i = target.getAdapterPosition();
-                int i2 = viewHolder.getAdapterPosition();
-
-                if (fingerUp){
-
-                    arrayList.set(i, arrayList.get(i2));
-                    tasksListRecyclerViewAdapter.notifyItemMoved(i2, i);
-                    String s = "";
-                    for (int ii=0; ii<arrayList.size(); ii++ ) s += arrayList.get(ii);
-                    Log.d("DTAG", "onMove: " + s);
-                }
-
-                tasksListRecyclerViewAdapter.notifyItemMoved(i2, i);
-
-
-
-
-
-
+                tasksListRecyclerViewAdapter.notifyItemMoved(fromPosition, toPosition);
                 return true;
             }
 
-            @Override
-            public void onMoved(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, int fromPos, RecyclerView.ViewHolder target, int toPos, int x, int y) {
-//                super.onMoved(recyclerView, viewHolder, fromPos, target, toPos, x, y);
-            }
+            private void reallyMoved(int from, int to) {arrayList.add(to, arrayList.remove(from));}
 
             @Override
             public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
                 int position = viewHolder.getAdapterPosition();
                 arrayList.remove(position);
+            }
 
-//                tasksListRecyclerViewAdapter.notifyItemRemoved(position);
+            @Override
+            public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                super.clearView(recyclerView, viewHolder);
+
+                if(dragFrom != -1 && dragTo != -1 && dragFrom != dragTo) {
+                    reallyMoved(dragFrom, dragTo);
+                    if (!arrayList.equals(arrayListCopy)){
+                        Log.d("DTAG", "from index " + dragFrom + " ,to index " + dragTo);
+                        Log.d("DTAG", "OLD " + Arrays.toString(((ArrayList)arrayListCopy).toArray()));
+                        Log.d("DTAG", "NEW " + Arrays.toString(arrayList.toArray()));
+                        arrayListCopy=arrayList.clone();
+                    }
+                }
+                dragFrom = dragTo = -1;
             }
         };
-
-
-
         return simpleItemTouchCallback;
     }
 }
-
-
-//https://medium.com/@fanfatal/android-swipe-menu-with-recyclerview-8f28a235ff28
-//https://stackoverflow.com/questions/35920584/android-how-to-catch-drop-action-of-itemtouchhelper-which-is-used-with-recycle
